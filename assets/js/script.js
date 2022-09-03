534067fe870f5d2520db9f33a771e7c7cadd0b3f
Rick South <rick@natamus.com>
2022-09-03 23:36:24 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var url = document.URL + "";
+	url = url.replaceAll("?p=/", "");
 
 	loadJSON('/urls/shortened_urls.json',
 		function(data) {
-			console.log(url);
-			console.log(data); 
+			var from = url.split("link/")[1]
+
+			if (from in data) {
+				var to = data[from];
+				console.log(from);
+				console.log(to);
+			}
 		},
 		function(xhr) { console.error(xhr); }
 	);
